692a5976953a16f5a0528405b2c6f3586f949a36
Jacksonbaker323 <Jacksonbaker323@Gmail.com>
2024-09-03 17:37:46 -0700

    Temp table exercises for chapter 1

--- ch1/temp_table.c
+++ ch1/temp_table.c
@@ -14,7 +14,7 @@ int main() {
 
   while (fahr <= upper) {
     celsius = 5 *  (fahr - 32) / 9;
-    printf("%d\t%d\n", fahr, celsius);
+    printf("%3d\t%6d\n",/* Can I put a comment here? */ fahr, celsius);
     fahr = fahr + step;
   }
 }
